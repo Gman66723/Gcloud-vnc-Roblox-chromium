3c4a3755e806e1c05ff29c2ec72cdf7ae76a2c55
Gman66723 <just_aperson22@outlook.com>
2025-09-16 08:38:37 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ echo "Installation complete."
 
 echo "Setting up VNC server..."
 mkdir -p ~/.vnc
-echo "password" | vncpasswd -f > ~/.vnc/passwd
+echo "Setting password to "password"" | vncpasswd -f > ~/.vnc/passwd
 chmod 600 ~/.vnc/passwd
 
 echo "Starting VNC server..."
@@ -22,3 +22,5 @@ echo "Starting noVNC..."
 websockify --web /usr/share/novnc/ 8080 localhost:5901 &
 
 echo "Setup complete. You can now connect to http://localhost:8080"
+echo "if you want to access go to Web preview > preview port 8080 > vnc.html or vnc_lite.html"
+echo "mod by gg"
